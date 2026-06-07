8de426e5e32020678cf49a024318e34d35760cef
astacatalyst <priyamvadhanune@gmail.com>
2026-06-07 23:30:09 +0530

    training on new dataset

--- train.py
+++ train.py
@@ -3,7 +3,7 @@ from ultralytics import YOLO
 model = YOLO("yolov8n.pt")
 
 model.train(
-    data="dataset/four clover -leaf/data.yaml",
+    data="dataset 2\Clover.v1i.yolov8/data.yaml",
     epochs = 50,
     imgsz = 640
 )
